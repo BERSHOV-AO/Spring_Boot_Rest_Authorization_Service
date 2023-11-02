7c9783901e08b53e76dafd718ca8e84c9dfaa8c3
Aleksey Bershov <bershov.aleksey@bk.ru>
2023-11-02 15:53:33 +0300

    Initial commit + intermediate code

---
+++ src/main/java/ru/netology/authorizationservice/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package ru.netology.authorizationservice.repository;
+
+
+import ru.netology.authorizationservice.Authorities;
+import ru.netology.authorizationservice.controller.AuthorizationController;
+
+import java.util.List;
+
+
+public class UserRepository {
+
+    private String user;
+    private String password;
+
+    public List<Authorities> getUserAuthorities(String user, String password) {
+
+    }
+
+}
+
---
+++ src/main/java/ru/netology/authorizationservice/service/AuthorizationService.java
@@ -0,0 +1,40 @@
+package ru.netology.authorizationservice.service;
+
+
+import ru.netology.authorizationservice.Authorities;
+import ru.netology.authorizationservice.repository.UserRepository;
+
+import java.util.List;
+
+public class AuthorizationService {
+
+    UserRepository userRepository;
+
+    List<Authorities> getAuthorities(String user, String password) {
+
+    }
+}
+
+
+//public class AuthorizationService {
+//    UserRepository userRepository;
+//
+//    List<Authorities> getAuthorities(String user, String password) {
+//        if (isEmpty(user) || isEmpty(password)) {
+//            throw new InvalidCredentials("User name or password is empty");
+//        }
+//        List<Authorities> userAuthorities = userRepository.getUserAuthorities(user, password);
+//        if (isEmpty(userAuthorities)) {
+//            throw new UnauthorizedUser("Unknown user " + user);
+//        }
+//        return userAuthorities;
+//    }
+//
+//    private boolean isEmpty(String str) {
+//        return str == null || str.isEmpty();
+//    }
+//
+//    private boolean isEmpty(List<?> str) {
+//        return str == null || str.isEmpty();
+//    }
+//}
